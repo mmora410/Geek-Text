75632e7c18e97dd1813866b2c5e101dfceb470d8
mmora410 <112897529+mmora410@users.noreply.github.com>
2022-10-12 21:38:51 -0400

    Add files via upload

--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -1,16 +1,26 @@
+
 # myapi/urls.py
 from django.urls import include, path
 from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'Wishlists', views.WishlistsViewSet)
-router.register(r'Books', views.BooksViewSet)
-router.register(r'Genres', views.GenresViewSet)
+router.register(r'Wishlists', views.WishlistsViewSet, 'Wishlists')
+router.register(r'Addresses', views.AddressesViewSet, 'Addresses')
+router.register(r'Authors', views.AuthorsViewSet, 'Authors')
+router.register(r'Books', views.BooksViewSet, 'Books')
+router.register(r'Genres', views.GenresViewSet, 'Genres')
+router.register(r'Bookratings', views.BookratingsViewSet, 'Bookratings')
+router.register(r'Purchasedbooks', views.PurchasedbooksViewSet, 'Purchasedbooks')
+router.register(r'Users', views.UsersViewSet, 'Users')
+router.register(r'Creditcards', views.CreditcardsViewSet, 'Creditcards')
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls))
 ]
+
+    
